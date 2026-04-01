8e2141cc58937327cd810f0133822e1099c7c708
tamijuni33@gmail.com <foicarafael@gmail.com>
2026-04-01 23:38:18 +0300

    api

--- GymRatService/Common/DTOs/GeminiDTOs/GeminiDto.cs
+++ GymRatService/Common/DTOs/GeminiDTOs/GeminiDto.cs
@@ -1,6 +1,29 @@
 ﻿namespace GymRatService.Common.DTOs.GeminiDTOs
 {
-    public class Class
+    public class ChatRequestDto
     {
+        public string Message { get; set; }
+    }
+    // Cum cerem de la Google
+    public class GeminiRequest
+    {
+        public object[] contents { get; set; }
+    }
+    // Cum ne răspunde Google
+    public class GeminiResponse
+    {
+        public Candidate[] candidates { get; set; }
+    }
+    public class Candidate
+    {
+        public Content content { get; set; }
+    }
+    public class Content
+    {
+        public Part[] parts { get; set; }
+    }
+    public class Part
+    {
+        public string text { get; set; }
     }
 }
